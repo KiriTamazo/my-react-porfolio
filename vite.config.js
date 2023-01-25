e6c14ffb71e937409c2e34f40ac6ac33bd459eb0
username <kiritokirikaya80@gmail.com>
2023-01-25 14:04:44 +0630

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -7,5 +9,26 @@ export default defineConfig({
   server: {
     port: 3001,
     open: "/public/index.html",
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.jsx"),
+      name: "MyLib",
+      // the proper extensions will be added
+      fileName: "my-lib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+        },
+      },
+    },
   },
 });
